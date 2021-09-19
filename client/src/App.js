823cf871b398969be5bc4ec0c45a06fb706af8ba
Ramin Hbb <raminhbb.au@gmail.com>
2021-09-19 10:14:36 +0430

    add login/signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import Login from './pages/login'
+import SignUp from './pages/sigup'
+
 
 function App() {
 
   return (
-    <div>Welcome to GymOnline</div>
+    <Login></Login>
   );
 }
 

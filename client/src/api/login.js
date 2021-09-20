83cf40a786cda13a6ff562de0baeff1830c4e887
raminhbbAU <raminhbb.au@gmail.com>
2021-09-21 00:39:29 +0430

    work on login api call

--- client/src/api/login.js
+++ client/src/api/login.js
@@ -1,5 +1,33 @@
-const login = (userName,password) => {
+const login = async(userName,password,type) => {
+    switch(type){
+        case "Gym":
+            return await gymLogin(userName,password);
+            break;
+        case "Trainer":
+            return await trainerLogin(userName,password);
+            break;
+        case "Athlete":
+            return await athleteLogin(userName,password);
+            break;
+        default:
+            throw 'The usertype is not defined';
+            break;
+    }
+}
 
+const gymLogin =  async(userName,password) => {
+    console.log("gymLogin called");
+    return 1;
+}
+
+const trainerLogin =  async(userName,password) => {
+    throw 'this service is unreachable now.'
+    return 1;
+}
+
+const athleteLogin =  async(userName,password) => {
+    throw 'this service is unreachable now.'
+    return 1;
 }
 
 export default {login};
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,48 +1,48 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import loginback from '../assests/images/loginback.jpg';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import loginback from "../assests/images/loginback.jpg";
+
+import Copyright from "../components/copyright.js"
+import loginAPI from "../api/login.js";
+
+
 
 const theme = createTheme();
 
 export default function SignInSide() {
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+
+    try {
+      console.log(data.get('userType'));
+      let value =  loginAPI.login(data.get('username'),data.get('password'),data.get('userType'));
+      console.log(value);
+    } catch (error) {
+      console.log(error);
+    }
+
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -52,12 +52,14 @@ export default function SignInSide() {
           sx={{
             // backgroundImage: 'url(https://source.unsplash.com/random)',
             backgroundImage: `url(${loginback})`,
-            opacity: '0.25',
-            backgroundRepeat: 'no-repeat',
+            opacity: "0.25",
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -65,18 +67,23 @@ export default function SignInSide() {
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
                 margin="normal"
                 required
@@ -97,10 +104,28 @@ export default function SignInSide() {
                 id="password"
                 autoComplete="current-password"
               />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
+
+              <RadioGroup
+                row
+                aria-label="userType"
+                defaultValue="Gym"
+                name="userType"
+              >
+                <FormControlLabel value="Gym" control={<Radio />} label="Gym" />
+                <FormControlLabel
+                  // disabled
+                  value="Trainer"
+                  control={<Radio />}
+                  label="Trainer"
+                />
+                <FormControlLabel
+                  disabled
+                  value="Athlete"
+                  control={<Radio />}
+                  label="Athlete"
+                />
+              </RadioGroup>
+
               <Button
                 type="submit"
                 fullWidth
@@ -109,6 +134,10 @@ export default function SignInSide() {
               >
                 Sign In
               </Button>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
               <Grid container>
                 <Grid item xs>
                   <Link href="#" variant="body2">

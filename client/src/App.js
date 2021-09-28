e4e504abe9a93852ad7f4363e0e589d7744c44ee
raminhbbAU <raminhbb.au@gmail.com>
2021-09-28 22:01:55 +0330

    add simple grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
+import SimpleGrid from './components/simpleGrid';
 import Dashboard from './pages/dashboard/dashboard';
 import Login from './pages/login/login.js'
 import SignUp from './pages/sigup/sigup.js'
 
 
 function App() {
-
   return (
-    <Login></Login>
+    <SimpleGrid title={'ramin grid'} datatable={randomData}/>
   );
 }
 

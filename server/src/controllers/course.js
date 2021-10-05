1b2ecca3935c9385b62f09524b11272d9a593d36
raminhbbAU <raminhbb.au@gmail.com>
2021-10-05 23:12:56 +0330

    course endpoint

--- server/src/controllers/course.js
+++ server/src/controllers/course.js
@@ -1,104 +1,226 @@
-const {router,models,bcrypt,jwt,authToken,yupValidator} = require('./index')
-const { gymRegisterSchema, gymLoginSchema } = require('../validationSchema/yup.validation.js');
+const {
+  router,
+  models,
+  bcrypt,
+  jwt,
+  authToken,
+  yupValidator,
+} = require("./index");
+const {
+  gymRegisterSchema,
+  gymLoginSchema,
+} = require("../validationSchema/yup.validation.js");
 
- router.post('/register',yupValidator(gymRegisterSchema), async (req,res,next) => {
-    
-    // Get user input
-    const { CourseName,CourseDescription,Trainer,Active,StartDate,EndDate,TrainerPercent,CourseType,PerSessionCost} = req.body;
+router.post("/register", authToken, async (req, res, next) => {
+  // Get user input
+  const {
+    CourseName,
+    CourseDescription,
+    Trainer,
+    Active,
+    StartDate,
+    EndDate,
+    TrainerPercent,
+    CourseType,
+    PerSessionCost,
+  } = req.body;
 
-     // check if user already exist
-     const oldCourse = await models.course.findOne({
-        where:{
-            Str_CourseName:CourseName
-        }
+  // check if course already exist
+  const oldCourse = await models.course.findOne({
+    where: {
+      Str_CourseName: CourseName,
+    },
+  });
+
+  if (oldCourse) {
+    return res.status(409).json({
+      res: false,
+      data: "This course name is already exist. please edit it or try to choose another unique name.",
+    });
+  }
+
+  models.course
+    .create({
+      Str_CourseName: CourseName,
+      Str_CourseDescription: CourseDescription,
+      Frk_Trainer: Trainer,
+      Bit_Active: Active,
+      Str_StartDate: StartDate,
+      Str_EndDate: EndDate,
+      Int_TrainerPercent: TrainerPercent,
+      Int_CourseType: CourseType,
+      Int_PerSessionCost: PerSessionCost,
+    })
+    .then((course) => {
+      res.status(200).json({
+        res: true,
+        data: course,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({
+        res: false,
+        data: "something wrong happend during registering new course. Please try again a bit later!",
+      });
+    });
+});
+
+router.put("/edit", authToken, async (req, res, next) => {
+  // Get user input
+  const {
+    id,
+    CourseName,
+    CourseDescription,
+    Trainer,
+    Active,
+    StartDate,
+    EndDate,
+    TrainerPercent,
+    CourseType,
+    PerSessionCost,
+  } = req.body;
+
+  // check if course already exist
+  const oldCourse = await models.course.findOne({
+    where: {
+      Prk_Course: id,
+    },
+  });
+
+  if (!oldCourse) {
+    return res.status(409).json({
+      res: false,
+      data: "The specific course doesn't exist! it must've deleted before",
+    });
+  }
+
+  //TOOD: check class activity, it can't be deleted if there were established seasions.
+
+  oldCourse
+    .update({
+      Str_CourseName: CourseName,
+      Str_CourseDescription: CourseDescription,
+      Frk_Trainer: Trainer,
+      Bit_Active: Active,
+      Str_StartDate: StartDate,
+      Str_EndDate: EndDate,
+      Int_TrainerPercent: TrainerPercent,
+      Int_CourseType: CourseType,
+      Int_PerSessionCost: PerSessionCost,
+    })
+    .then((updatedrecord) => {
+      res.status(200).json({
+        res: true,
+        data: updatedrecord,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({
+        res: false,
+        data: "something wrong happend during editing course. Please try again a bit later!",
+      });
+    });
+});
+
+router.delete("/delete", authToken, async (req, res, next) => {
+  // Get user input
+  const { id } = req.body;
+
+  // check if course already exist
+  const oldCourse = await models.course.findOne({
+    where: {
+      Prk_Course: id,
+    },
+  });
+
+  if (!oldCourse) {
+    return res.status(409).json({
+      res: false,
+      data: "The specific course doesn't exist! it must've deleted before",
+    });
+  }
+
+  oldCourse
+    .destroy()
+    .then((rowDeleted) => {
+      if (rowDeleted === 1) {
+        res.status(200).json({
+          res: true,
+          data: updatedrecord,
+        });
+      } else {
+        res.status(200).json({
+          res: 500,
+          data: "",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({
+        res: false,
+        data: "something wrong happend during deleting course. Please try again a bit later!",
+      });
+    });
+});
+
+router.post("/registerOffTime", authToken, async (req, res, next) => {
+  // Get user input
+  const { Course, Date, StartTime, EndTime, Reason } = req.body;
+
+  // check if off time already exist
+  const oldOFFTime = await models.courseofftimes.findOne({
+    where: {
+      Frk_Course: Course,
+      Str_Date: Date,
+      Str_StartTime: StartTime,
+      Str_EndTime: EndTime,
+    },
+  });
+
+  if (oldOFFTime) {
+    return res.status(409).json({
+      res: false,
+      data: "This course off time is already exist. please edit it or try to choose another unique time period.",
+    });
+  }
+
+  models.courseofftimes
+    .create({
+        Frk_Course: Course,
+        Str_Date: Date,
+        Str_StartTime: StartTime,
+        Str_EndTime: EndTime,
+        Str_Reason:Reason,
+    })
+    .then((courseofftimes) => {
+      res.status(200).json({
+        res: true,
+        data: courseofftimes,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({
+        res: false,
+        data: "something wrong happend during registering new course off time. Please try again a bit later!",
+      });
     });
 
-     if (oldCourse) {
-        return res.status(409).send("This course name is already exist. please edit it or try to choose another unique name.");
-     }
+});
 
+router.put("/editOffTime", authToken, async (req, res, next) => {
+  res.send("the editOffTime API called");
+});
 
-    models.course.create({
-        Str_CourseName:CourseName,
-        Str_CourseDescription:CourseDescription,
-        Frk_Trainer:Trainer,
-        Bit_Active:Active,
-        Str_StartDate:StartDate,
-        Str_EndDate:EndDate,
-        Int_TrainerPercent:TrainerPercent,
-        Int_CourseType:CourseType,
-        Int_PerSessionCost:PerSessionCost
-     
-    }).then( (course) => {
+router.delete("/deleteOffTime", authToken, async (req, res, next) => {
+  res.send("the deleteOffTime API called");
+});
 
-            res.status(200).json({
-                res:true,
-                data:course
-            })
+router.get("/getByGymID", authToken, async (req, res, next) => {
+  res.send("the getByGymID API called");
+});
 
-     }).catch( (error) => {
-        console.log(error);
-        return res.status(500).send('There was a problem registering the user.');
-    
-     })
-
-})
-
-router.post('/edit',yupValidator(gymLoginSchema), async (req,res,next) => {
-    
-    // Get user input
-    const { UserName, Password } = req.body;
-
-    // check if user already exist
-    const oldGym = await models.gym.findOne({
-        where:{
-            Str_UserName:UserName
-        }
-    });
-
-
-    // Check User & Password
-    if (oldGym && (await bcrypt.compare(Password,oldGym.Str_Password) ) ) {
-        
-            // create new token
-            let token = jwt.sign({ id: oldGym.Prk_Gym_AutoID,username:oldGym.Str_UserName}, process.env.JWT_SECRET, {
-                expiresIn: process.env.JWT_EXPIRES_IN
-              });
-    
-            if (!token) return res.status(500).send('There was a problem during token generation');
-    
-            res.status(200).json({
-                auth:true,
-                token,
-                data:oldGym
-            })
-        
-    }
-    else
-    {
-        return res.status(409).send("User or Password is wrong. Please Try again");
-    }
-
-})
-
-router.delete('/delete',authToken,async(req,res,next) =>{
-    res.send('the delete API called');
-})
-
-router.post('/registerOffTime',authToken,async(req,res,next) =>{
-    res.send('the registerOffTime API called');
-})
-
-router.put('/editOffTime',authToken,async(req,res,next) =>{
-    res.send('the editOffTime API called');
-})
-
-router.delete('/deleteOffTime',authToken,async(req,res,next) =>{
-    res.send('the deleteOffTime API called');
-})
-
-router.get('/getByGymID',authToken,async(req,res,next) =>{
-    res.send('the getByGymID API called');
-})
-
- module.exports = router;  
+module.exports = router;

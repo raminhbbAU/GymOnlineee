7395f6754cb716eeebdf8b573e3b11a895f1378d
Ramin Hbb <raminhbb.au@gmail.com>
2022-01-03 18:02:19 +0330

    dashboard

--- client/src/pages/dashboard/DashboardApp.js
+++ client/src/pages/dashboard/DashboardApp.js
@@ -18,22 +18,33 @@ export default function DashboardApp() {
   return (
     <Page title="Dashboard | Minimal-UI">
       <Container maxWidth="xl">
-        <Box sx={{ pb: 5 }}>
-          <Typography variant="h4" sx={{ mb: 5 }}>Hi {logininfo.Str_GymName}! Welcome back</Typography>
+        <Box sx={{ pb: 5 }}> 
+          <Typography variant="h3" sx={{ mb: 5 }}>Hi {logininfo.Str_GymName}! Welcome back</Typography>
 
           <Grid container spacing={3}>
             <Grid item xs={12} sm={6} md={3}>
-              <CustomDashboardBox title={"Students"} value={50} color={palette.secondary} />
+              <CustomDashboardBox title={"Students"} value={50} color={palette.success} />
             </Grid>
             <Grid item xs={12} sm={6} md={3}>
-              <CustomDashboardBox title={"Courses"} value={50} color={palette.info} />
+              <CustomDashboardBox title={"Courses"} value={50} color={palette.error} />
             </Grid>
             <Grid item xs={12} sm={6} md={3}>
-              <CustomDashboardBox title={"Monthly Income"} value={50} color={palette.primary} />
+              <CustomDashboardBox title={"Monthly Income"} value={50} color={palette.info} />
             </Grid>
             <Grid item xs={12} sm={6} md={3}>
-              <CustomDashboardBox title={"Student/Athlete"} value={50} color={palette.warning}/>
+              <CustomDashboardBox title={"Total Reminder"} value={50} color={palette.warning}/>
             </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12} sx={{ mt: 8 }}>
+            <Typography variant="h5" sx={{ mb: 2 }}>Upcoming Events</Typography>
+            <CustomDashboardBox title={"Total Reminder"} value={50} color={palette.warning}/>
+          </Grid>
+
+
+          <Grid item xs={12} md={12} lg={12} sx={{ mt: 8 }}>
+            <Typography variant="h5" sx={{ mb: 2 }}>Debtor Students</Typography>
+            <CustomDashboardBox title={"Total Reminder"} value={50} color={palette.warning}/>
           </Grid>
 
         </Box>

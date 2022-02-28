122df034303f8feaddb606a25905d9d98a4101d6
raminhbbAU <raminhbb.au@gmail.com>
2022-02-28 21:22:11 +0330

    delete extra files

--- client/src/constants/api.config.js
+++ client/src/constants/api.config.js
@@ -1,3 +1,3 @@
-const apiPath = 'http://localhost:3001/api/v1/';
-
+//const apiPath = 'http://localhost:3001/api/v1/';
+const apiPath = 'http://193.151.130.19:3001/api/v1/';
 export default apiPath;
